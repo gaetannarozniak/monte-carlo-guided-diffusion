9074f05116e6865a4965097b9b3aba25d34b1faa
gaetannarozniak <gaetan.narozniak@gmail.com>
2025-04-04 14:59:05 +0200

    a beginning of dinosaur

--- ddpm.py
+++ ddpm.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.optim import AdamW
+import matplotlib.pyplot as plt
 
 class DDPM():
     def __init__(self, n: int, net: nn.Module, input_shape):
@@ -20,20 +21,21 @@ class DDPM():
     def embed_time(self, X, t):
         assert len(X.shape) == 2
         t_normalized = (2*t-self.n)/self.n
-        input = torch.cat((X, t_normalized*torch.ones((X.shape[0], 1))), dim=1)
+        input = torch.cat((X, t_normalized), dim=1)
         return input
 
 
     def train(self, loader, n_epochs):
+        epoch_loss = []
         optimizer = AdamW(self.net.parameters())
         for e in range(n_epochs):
             lossi = []
             for batch in loader:
                 X = batch[0] # (batch_size, ...)
-                t = np.random.randint(1, self.n+1)
+                t = torch.randint(1, self.n, (X.shape[0],1))
                 noise = torch.randn_like(X) 
-                X_noised = np.sqrt(self.alpha_bars[t])*X + \
-                    np.sqrt(1-self.alpha_bars[t])*noise
+                X_noised = torch.sqrt(self.alpha_bars[t])*X + \
+                    torch.sqrt(1-self.alpha_bars[t])*noise
                 input = self.embed_time(X_noised, t)
                 out = self.net(input)
                 assert out.shape == noise.shape, f"{out.shape = }, {noise.shape = }"
@@ -43,12 +45,14 @@ class DDPM():
                 optimizer.step()
                 lossi.append(loss.item())
             print(f"epoch {e}: mean loss = {np.mean(lossi)}")
+            epoch_loss.append(np.mean(lossi))
+        plt.plot(epoch_loss)
 
-    def sample(self, n_samples):
+    def sample(self, n_samples, eta=1):
         x = torch.randn((n_samples,) + self.input_shape)
         for t in range(self.n, 0, -1):
-            noise = torch.randn_like(x)
-            input = self.embed_time(x, t)
-            noise_pred = self.net(input) * self.betas[t]/np.sqrt(1-self.alpha_bars[t])
-            x = (x - noise_pred)/np.sqrt(self.alphas[t]) + self.sigmas[t] * noise
+            noise = eta*torch.randn_like(x)
+            input = self.embed_time(x, t*torch.ones(x.shape[0], 1))
+            noise_pred = self.net(input) * self.betas[t]/torch.sqrt(1-self.alpha_bars[t])
+            x = (x - noise_pred)/torch.sqrt(self.alphas[t]) + self.sigmas[t] * noise
         return x
