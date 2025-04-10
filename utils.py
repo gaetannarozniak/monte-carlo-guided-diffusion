714cbde4159d9b9268db66888418c43be1511b0c
gaetannarozniak <gaetan.narozniak@gmail.com>
2025-04-10 14:06:50 +0200

    generating images with celeba

---
+++ utils.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_image(tensor_image):
+    if tensor_image.shape[0] == 1:
+        tensor_image = tensor_image.squeeze(0)
+    # renormalize between 0 and 1
+    tensor_image = (tensor_image*0.5)+0.5
+    tensor_image = tensor_image.permute(1, 2, 0)
+    np_image = tensor_image.detach().cpu().numpy()
+    plt.imshow(np_image)
